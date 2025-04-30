23d68948c77527e2f1a843732143dc9f7d8cc14c
MishaFakhar <129263968+MishaFakhar@users.noreply.github.com>
2025-04-30 23:57:42 +0500

    Updated code

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def index():
                 
                 encrypted_message = request.form.get("encrypted_message")
                 
-                print(f"Decryption - Using saved Shared Secret (hex): {shared_secret.hex()}")
+                print(f"Decryption - Using saved shared Secret (hex): {shared_secret.hex()}")
                 print(f"Deecryption - AES Key (hex): {aes_key.hex()}")
 
                 decrypted_message = aes_decrypt(aes_key, encrypted_message)
